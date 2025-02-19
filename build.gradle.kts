7540e4d10d60d73adab4e31663c340eaa013cb39
Taras Yurkovskyi <tarasyurkovskti@Tarass-MacBook-Pro.local>
2025-02-19 14:14:39 +0200

    empty

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,5 @@ allprojects {
     version = "0.0.1-SNAPSHOT"
     repositories {
         mavenCentral()
-
-        maven {
-            credentials(AwsCredentials::class.java) {
-                accessKey = extra["AWS_ACCESS_KEY_ID"].toString()
-                secretKey = extra["AWS_SECRET_ACCESS_KEY"].toString()
-            }
-            url = uri(extra["repository"].toString())
-        }
     }
 }
